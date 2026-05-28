cf89638ae62e4b11c5818b69247619768b1fd264
harrylarryxyz <harrylarryxyz@users.noreply.github.com>
2026-05-28 10:08:16 +0800

    fix: provide prisma to storage module

--- apps/api/src/storage/storage.module.ts
+++ apps/api/src/storage/storage.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { LocalStorageService } from './local-storage.service';
 import { StorageController } from './storage.controller';
 
-@Module({ controllers: [StorageController], providers: [LocalStorageService], exports: [LocalStorageService] })
+@Module({ controllers: [StorageController], providers: [LocalStorageService, PrismaService], exports: [LocalStorageService] })
 export class StorageModule {}
